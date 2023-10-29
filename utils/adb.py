54b8b865d4b77f21691c531cfd3492aef4487be7
auh <21078607+fanlushuai@users.noreply.github.com>
2023-10-29 08:48:19 +0800

    fix

--- utils/adb.py
+++ utils/adb.py
@@ -1,6 +1,6 @@
 import adbutils
 from adbutils import adb
-import re,os,glob
+import glob
 # https://github.com/openatx/adbutils  要求python3.6 
 # https://blog.csdn.net/xiaoerbuyu1233/article/details/122124201
 
@@ -20,9 +20,9 @@ for d in adb.device_list():
     # 屏幕常亮？？ https://www.jianshu.com/p/bae933aac556   在我的手机上，不管用。。。妈的
     d.shell("svc power stayon usb")
 
-    for f in glob.glob(r'E:\code\open\kuaishou\**\*.js',recursive=True):
+    # glob的 ** 在windows下好像不管用？？？！！！
+    for f in glob.glob('E:\code\open\kuaishou-autojs\**\*.js',recursive=True):
         localPath=f
-        phonePath=f.replace("E:\code\open\kuaishou","/storage/emulated/0/脚本/kuaishou").replace("\\","/")
-        print(localPath)
-        print(phonePath)
+        phonePath=f.replace("E:\code\open\kuaishou-autojs","/storage/emulated/0/脚本/ks").replace("\\","/")
+        print(localPath+"   >>>   "+phonePath)
         d.sync.push(localPath,phonePath)
