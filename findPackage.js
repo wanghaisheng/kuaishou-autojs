d6b67703e69145aca9e20cf60570e1fbf20871ba
auh <21078607+fanlushuai@users.noreply.github.com>
2023-11-04 06:05:30 +0800

    app启动不显示主界面

--- findPackage.js
+++ findPackage.js
@@ -1,2 +1,35 @@
+// 找红包
+// 通过某个列表进入工作流程。开始滑动，每次等待几秒，进行包的寻找，如果存在，获取时间，人数，并且分享出去。循环。
 
+// 红包分析。红包类型：倒计时红包，进行中红包，自动弹出红包，多个红包。
+
+// 首页-关注-找到直播-开始滑动
+function intoList(){
+
+}
+
+function reward(){
+
+} 
+
+function getRewardTime(){
+
+}
+
+function getPersonCount(){
+
+}
+
+function share(){
+
+}
+
+function send2Server(){
+
+}
+
+// 抢红包
+function get(){
+
+}
 
